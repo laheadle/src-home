ec6e7cf2a467db0e092be74f889a9f6d2e146d23
Lyn Headley <laheadle@gmail.com>
2020-02-29 19:02:42 -0500

    c

--- elisp/org-mode.el
+++ elisp/org-mode.el
@@ -112,8 +112,7 @@
 
 ;; Custom agenda command definitions
 (setq org-agenda-custom-commands
-      (quote (("y" agenda*)
-              ("g" "Goals"
+      (quote (("y" agenda*)("g" "Goals"
                ((tags "TODO=\"GOAL\"" nil)
                 (tags "GOALS-TODO=\"GOAL\""
                       ((org-tags-match-list-sublevels nil)))
@@ -387,13 +386,6 @@ A prefix arg forces clock in of the default task."
 ;; Show all agenda dates - even if they are empty
 (setq org-agenda-show-all-dates t)
 
-;; Sorting order for tasks on the agenda
-(setq org-agenda-sorting-strategy
-      (quote ((agenda habit-down time-up user-defined-up effort-up category-keep)
-              (todo tsia-up)
-              (tags category-up effort-up)
-              (search category-up))))
-
 ;; Start the weekly agenda on Monday
 (setq org-agenda-start-on-weekday 1)
 
@@ -404,99 +396,6 @@ A prefix arg forces clock in of the default task."
 
 ;; Display tags farther right
 (setq org-agenda-tags-column -102)
-
-;;
-;; Agenda sorting functions
-;;
-(setq org-agenda-cmp-user-defined 'bh/agenda-sort)
-
-(defun bh/agenda-sort (a b)
-  "Sorting strategy for agenda items.
-Late deadlines first, then scheduled, then non-late deadlines"
-  (let (result num-a num-b)
-    (cond
-     ; time specific items are already sorted first by org-agenda-sorting-strategy
-
-     ; non-deadline and non-scheduled items next
-     ((bh/agenda-sort-test 'bh/is-not-scheduled-or-deadline a b))
-
-     ; deadlines for today next
-     ((bh/agenda-sort-test 'bh/is-due-deadline a b))
-
-     ; late deadlines next
-     ((bh/agenda-sort-test-num 'bh/is-late-deadline '> a b))
-
-     ; scheduled items for today next
-     ((bh/agenda-sort-test 'bh/is-scheduled-today a b))
-
-     ; late scheduled items next
-     ((bh/agenda-sort-test-num 'bh/is-scheduled-late '> a b))
-
-     ; pending deadlines last
-     ((bh/agenda-sort-test-num 'bh/is-pending-deadline '< a b))
-
-     ; finally default to unsorted
-     (t (setq result nil)))
-    result))
-
-(defmacro bh/agenda-sort-test (fn a b)
-  "Test for agenda sort"
-  `(cond
-    ; if both match leave them unsorted
-    ((and (apply ,fn (list ,a))
-          (apply ,fn (list ,b)))
-     (setq result nil))
-    ; if a matches put a first
-    ((apply ,fn (list ,a))
-     (setq result -1))
-    ; otherwise if b matches put b first
-    ((apply ,fn (list ,b))
-     (setq result 1))
-    ; if none match leave them unsorted
-    (t nil)))
-
-(defmacro bh/agenda-sort-test-num (fn compfn a b)
-  `(cond
-    ((apply ,fn (list ,a))
-     (setq num-a (string-to-number (match-string 1 ,a)))
-     (if (apply ,fn (list ,b))
-         (progn
-           (setq num-b (string-to-number (match-string 1 ,b)))
-           (setq result (if (apply ,compfn (list num-a num-b))
-                            -1
-                          1)))
-       (setq result -1)))
-    ((apply ,fn (list ,b))
-     (setq result 1))
-    (t nil)))
-
-(defun bh/is-not-scheduled-or-deadline (date-str)
-  (and (not (bh/is-deadline date-str))
-       (not (bh/is-scheduled date-str))))
-
-(defun bh/is-due-deadline (date-str)
-  (string-match "Deadline:" date-str))
-
-(defun bh/is-late-deadline (date-str)
-  (string-match "\\([0-9]*\\) d\. ago:" date-str))
-
-(defun bh/is-pending-deadline (date-str)
-  (string-match "In \\([^-]*\\)d\.:" date-str))
-
-(defun bh/is-deadline (date-str)
-  (or (bh/is-due-deadline date-str)
-      (bh/is-late-deadline date-str)
-      (bh/is-pending-deadline date-str)))
-
-(defun bh/is-scheduled (date-str)
-  (or (bh/is-scheduled-today date-str)
-      (bh/is-scheduled-late date-str)))
-
-(defun bh/is-scheduled-today (date-str)
-  (string-match "Scheduled:" date-str))
-
-(defun bh/is-scheduled-late (date-str)
-  (string-match "Sched\.\\(.*\\)x:" date-str))
 
 ;; Use sticky agenda's so they persist
 (setq org-agenda-sticky t)
