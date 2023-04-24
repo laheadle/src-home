23214d2ca1049003c0d654926c55fc6e0a41eb7a
lyn.headley@mheducation.com <lyn.headley@mheducation.com>
2023-04-24 10:39:25 -0400

    c

--- elisp/custom.el
+++ elisp/custom.el
@@ -12,7 +12,6 @@
  '(magit-list-refs-sortby "-creatordate")
  '(magit-reset-arguments nil)
  '(package-selected-packages
-   '(ox-json orderless typescript-mode org-transclusion bookmark+ quelpa-use-package quelpa org-jira hydra counsel swiper transient exec-path-from-shell which-key free-keys ht dash org-contrib use-package))
    '(ox-json org-ml orderless typescript-mode org-transclusion bookmark+ quelpa-use-package quelpa org-jira hydra counsel swiper transient exec-path-from-shell which-key free-keys ht dash org-contrib use-package))
  '(undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo"))))
 (custom-set-faces
